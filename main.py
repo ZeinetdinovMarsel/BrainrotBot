c281704a7ba5751817b62ae783100ab49f323e23
ZeinetdinovMarsel <marsel.zeinetdinov@gmail.com>
2026-03-04 19:19:33 +0400

    10 мин

--- main.py
+++ main.py
@@ -115,7 +115,7 @@ def main():
 
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, register_by_word))
     app.add_handler(ChatMemberHandler(chat_member_update, chat_member_types=["my_chat_member"]))
-    app.job_queue.run_repeating(send_random_word, interval=1800, first=5)
+    app.job_queue.run_repeating(send_random_word, interval=600, first=600)
 
     print("Бот запущен...")
     app.run_polling()
